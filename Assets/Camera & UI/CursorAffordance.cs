8346a5338b9faa7c4631e6075344e02bce88422e
Jordan Wong <jordanwongwk@gmail.com>
2018-04-12 12:58:05 +0800

    Assigning cursor texture based on layer. [S1 L15 : Using Cursor Affordance]

---
+++ Assets/Camera & UI/CursorAffordance.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorAffordance : MonoBehaviour {
+
+	[SerializeField] Texture2D moveCursor = null;
+	[SerializeField] Texture2D enemyCursor = null;
+	[SerializeField] Texture2D unknownCursor = null;
+	[SerializeField] Vector2 cursorHotspot = new Vector2 (96, 96);
+
+	CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start () {
+		cameraRaycaster = GetComponent<CameraRaycaster> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (cameraRaycaster.layerHit) {
+			case Layer.Walkable:
+				Cursor.SetCursor (moveCursor, cursorHotspot, CursorMode.Auto);
+				break;
+			case Layer.Enemy:
+				Cursor.SetCursor (enemyCursor, cursorHotspot, CursorMode.Auto);
+				break;
+			case Layer.RaycastEndStop:
+				Cursor.SetCursor (unknownCursor, cursorHotspot, CursorMode.Auto);
+				break;
+			default:
+				Debug.Log ("No cursor assigned for this layer.");
+				return;
+		}
+	}
+}
