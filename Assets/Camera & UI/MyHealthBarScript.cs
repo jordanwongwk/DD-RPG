eb4623abe904b8a65a59874fdfb647cf1e662ea6
Jordan Wong <jordanwongwk@gmail.com>
2018-04-13 17:09:00 +0800

    Adding Health Bar UI for Player [S2 L29 : Player Health Bar UI]

---
+++ Assets/Camera & UI/MyHealthBarScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyHealthBarScript : MonoBehaviour {
+
+	Player player;
+	Image image;
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType<Player> ();
+		image = GetComponent<Image> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		image.fillAmount = player.healthAsPercentage;
+	}
+}
---
+++ Assets/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField] float maxHealthPoints = 100f;
+	[SerializeField] float currentHealthPoints = 100f;
+
+
+	public float healthAsPercentage
+	{
+		get
+		{
+			return currentHealthPoints / maxHealthPoints;	
+		}
+	}
+
+}
