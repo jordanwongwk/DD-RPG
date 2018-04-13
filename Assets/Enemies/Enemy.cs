8747a8da0f0ab1feb4749fe7f87f928b5605d264
Jordan Wong <jordanwongwk@gmail.com>
2018-04-13 18:00:00 +0800

    Added enemy HP UI [S2 L30 : Enemy Floating Health Bar]

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Enemy : MonoBehaviour {
 
 	[SerializeField] float maxHealthPoints = 100f;
 	[SerializeField] float currentHealthPoints = 100f;
@@ -14,5 +14,4 @@ public class Player : MonoBehaviour {
 			return currentHealthPoints / maxHealthPoints;	
 		}
 	}
-
 }
---
+++ Assets/Enemies/MyEnemyHealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MyEnemyHealthBar : MonoBehaviour {
+
+	Enemy enemy;
+	Image image;
+	// Use this for initialization
+	void Start () {
+		enemy = FindObjectOfType<Enemy> ();
+		image = GetComponent<Image> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		image.fillAmount = enemy.healthAsPercentage;
+	}
+}
