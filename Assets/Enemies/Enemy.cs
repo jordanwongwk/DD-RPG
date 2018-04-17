00759c9a6c4f1dd2b1205b4977dbd7cce981beca
Jordan Wong <jordanwongwk@gmail.com>
2018-04-17 16:58:30 +0800

    Adding navmesh and the enemy chase within certain range [S2 L44 : Make Enemies Pathfind to Player]

--- Assets/Enemies/Enemy.cs
+++ Assets/Enemies/Enemy.cs
@@ -1,17 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour {
 
 	[SerializeField] float maxHealthPoints = 100f;
 	[SerializeField] float currentHealthPoints = 100f;
+	[SerializeField] float triggerRadius = 5f;
+
+	AICharacterControl aiCharacterControl = null;
+	GameObject player;
 
 	public float healthAsPercentage
 	{
 		get
 		{
 			return currentHealthPoints / maxHealthPoints;	
+		}
+	}
+
+	void Start(){
+		aiCharacterControl = GetComponent<AICharacterControl> ();
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+	void Update(){
+		float distanceDiff = Vector3.Distance (player.transform.position, transform.position);
+
+		if (distanceDiff <= triggerRadius) {
+			aiCharacterControl.SetTarget (player.transform);
+		} else if (distanceDiff > triggerRadius){
+			aiCharacterControl.SetTarget (transform);
 		}
 	}
 }
