f76dfa0c8c95753b4587619ed1c9a7febcd916db
Jordan Wong <jordanwongwk@gmail.com>
2018-05-03 17:11:08 +0800

    Adding in Energy Component [ S3 L105 : Extension By Composition ]

---
+++ Assets/_Characters/Player/Energy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using RPG.CameraUI;
+
+namespace RPG.Characters {
+	public class Energy : MonoBehaviour {
+
+		[SerializeField] Image energyBar = null;
+		[SerializeField] float maxEnergyPoints = 100f;
+		[SerializeField] float pointsPerHit = 10f;
+
+		[SerializeField] float currentEnergyPoints;
+		CameraRaycaster cameraRaycaster;
+
+		void Start () {
+			currentEnergyPoints = maxEnergyPoints;
+			RegisterInDelegates ();
+		}
+
+		void RegisterInDelegates (){
+			cameraRaycaster = FindObjectOfType<CameraRaycaster> ();
+			cameraRaycaster.notifyRightClickObservers += ConsumeEnergy;
+		}
+
+		void ConsumeEnergy(RaycastHit raycastHit, int layerHit){
+			currentEnergyPoints = Mathf.Clamp (currentEnergyPoints - pointsPerHit, 0f, maxEnergyPoints);
+			energyBar.fillAmount = (currentEnergyPoints / maxEnergyPoints);
+		}
+	}
+}
