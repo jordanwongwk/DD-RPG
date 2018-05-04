31c33413874f2d7bf6fd9b8f6c053736ec402a2c
Jordan Wong <jordanwongwk@gmail.com>
2018-05-04 17:10:48 +0800

    Adding in some Special Abilities Scriptable Objects, Interface and Behaviour [ S3 L110 : Special Abilities System Overview ]

---
+++ Assets/_Characters/Special Abilities/SpecialAbilityConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Characters {
+	public abstract class SpecialAbilityConfig : ScriptableObject {
+
+		[Header("Special Attack General")]
+		[SerializeField] float energyCost = 10f;
+
+		abstract public ISpecialAbility AddComponent(GameObject gameObjectToAttachTo);
+	}
+}
