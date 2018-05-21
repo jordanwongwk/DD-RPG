2dbfd67a02bfbc762c8a712e1455af3a12c710ad
Jordan Wong <jordanwongwk@gmail.com>
2018-05-21 11:54:31 +0800

    Add an Enemy101 which inherits the new character system. Test before applying to all other enemies. [ S4 L161 : A Strong Character Basis ]

--- Assets/_Characters/Weapons/WeaponPickupPoint.cs
+++ Assets/_Characters/Weapons/WeaponPickupPoint.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 namespace RPG.Characters {
-	[ExecuteInEditMode]
+//	[ExecuteInEditMode]
 	public class WeaponPickupPoint : MonoBehaviour {
 		
 		[SerializeField] Weapon weaponConfig = null;
