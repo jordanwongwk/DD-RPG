bdfd3646dfef8409f5db9d4f46b05ebdf6a3ba7a
Jordan Wong <jordanwongwk@gmail.com>
2018-05-21 16:14:14 +0800

    Adding enum for AI Behaviour into EnemyAI [ S4 L165 : AI Behaviour Tree In Unity ]

--- Assets/_Characters/Enemies/EnemyAI.cs
+++ Assets/_Characters/Enemies/EnemyAI.cs
@@ -5,30 +5,56 @@ using UnityEngine;
 namespace RPG.Characters {
 	[RequireComponent(typeof(WeaponSystem))]
 	public class EnemyAI : MonoBehaviour{		
-		[SerializeField] float moveRadius = 6f;
+		[SerializeField] float chaseRadius = 6f;
 
-		bool isAttacking = false;
-		float attackRadius;
+		float currentWeaponRange;
+		float distanceToPlayer;
 		PlayerMovement player;
+		Character character;
+
+		enum State { idle, attacking, chasing, patroling }
+		State state = State.idle;
 
 		void Start(){
+			character = GetComponent<Character> ();
 			player = FindObjectOfType<PlayerMovement>();
 		}
 
 		void Update(){
-			float distanceDiff = Vector3.Distance (player.transform.position, transform.position);
+			distanceToPlayer = Vector3.Distance (player.transform.position, transform.position);
 			WeaponSystem weaponSystem = GetComponent<WeaponSystem> ();
-			attackRadius = weaponSystem.GetCurrentWeaponConfig ().GetMaxAttackRange ();
+			currentWeaponRange = weaponSystem.GetCurrentWeaponConfig ().GetMaxAttackRange ();
+
+			if (distanceToPlayer > chaseRadius && state != State.patroling) {
+				StopAllCoroutines ();
+				state = State.patroling;
+			}
+			if (distanceToPlayer <= chaseRadius && state != State.chasing) {
+				StopAllCoroutines ();
+				StartCoroutine(ChasePlayer());
+			}
+			if (distanceToPlayer <= currentWeaponRange && state != State.attacking) {
+				StopAllCoroutines ();
+				state = State.attacking;
+			}
+		}
+
+		IEnumerator ChasePlayer(){
+			state = State.chasing;
+			while (distanceToPlayer >= currentWeaponRange) {
+				character.SetDestination (player.transform.position);
+				yield return new WaitForEndOfFrame();
+			}
 		}
 
 		void OnDrawGizmos(){
 			// Drawing Attack Radius
 			Gizmos.color = new Color(255,0,0,0.5f);
-			Gizmos.DrawWireSphere (transform.position, attackRadius);
+			Gizmos.DrawWireSphere (transform.position, currentWeaponRange);
 
 			// Drawing Move Radius
 			Gizmos.color = new Color(0,255,0,0.5f);
-			Gizmos.DrawWireSphere (transform.position, moveRadius);
+			Gizmos.DrawWireSphere (transform.position, chaseRadius);
 		}
 	}
 }
