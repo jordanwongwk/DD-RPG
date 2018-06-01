7ee05365de34d2d60f0c1a2010f3afdc10644c15
Jordan Wong <jordanwongwk@gmail.com>
2018-06-01 13:17:51 +0800

    Giving boss AOE special ability. (Part 1: Activate it after certain time)

--- Assets/_Characters/Enemies/EnemyAI.cs
+++ Assets/_Characters/Enemies/EnemyAI.cs
@@ -6,10 +6,20 @@ namespace RPG.Characters {
 	[RequireComponent(typeof(WeaponSystem))]
 	[RequireComponent(typeof(HealthSystem))]
 	public class EnemyAI : MonoBehaviour{		
+		[Header("General")]
 		[SerializeField] float chaseRadius = 6f;
-		[SerializeField] WaypointContainer patrolPath;
+		[SerializeField] WaypointContainer patrolPath = null;
 		[SerializeField] float waypointTolerance = 2.0f;
 		[SerializeField] float waitAtWaypoint = 2.0f;
+
+		[Header("Skills")]
+		[SerializeField] AbilityConfig[] abilities = null;
+		[SerializeField] float timeWaitBetweenCasts = 5f;
+
+		const int ABILITY_NUMBER_0 = 0;
+
+		float timeLastCast;
+		bool isReadyToCastAbility = false;
 
 		int nextWaypointIndex;
 		float currentWeaponRange;
@@ -18,13 +28,21 @@ namespace RPG.Characters {
 		Character character;
 		WeaponSystem weaponSystem;
 
-		public enum State { idle, attacking, chasing, patroling }
+		public enum State { idle, attacking, chasing, patroling, castingAbility }
 		State state = State.idle;
 
 		void Start(){
 			character = GetComponent<Character> ();
 			weaponSystem = GetComponent<WeaponSystem> ();
 			player = FindObjectOfType<PlayerControl>();
+
+			SetupAbilitiesBehaviour ();
+		}
+
+		void SetupAbilitiesBehaviour () {
+			for (int abilityIndex = 0; abilityIndex < abilities.Length; abilityIndex++) {
+				abilities[abilityIndex].AttachAbilityTo (gameObject);
+			}
 		}
 
 		void Update(){
@@ -36,13 +54,19 @@ namespace RPG.Characters {
 				StopAllCoroutines ();
 				StartCoroutine (Patrol ());
 			}
-			if (distanceToPlayer <= chaseRadius && state != State.chasing) {
+			if (distanceToPlayer <= chaseRadius && state != State.chasing && !isReadyToCastAbility) {
 				StopAllCoroutines ();
 				StartCoroutine(ChasePlayer());
 			}
-			if (distanceToPlayer <= currentWeaponRange && state != State.attacking) {
+			if (distanceToPlayer <= currentWeaponRange && state != State.attacking && !isReadyToCastAbility) {
 				StopAllCoroutines ();
 				StartCoroutine (AttackPlayer ());
+			}
+			if (distanceToPlayer <= currentWeaponRange && state != State.castingAbility && isReadyToCastAbility) {
+				StopAllCoroutines ();
+				weaponSystem.StopAttacking ();
+				Debug.Log ("Ready");
+				StartCoroutine (CastAbility ());
 			}
 		}
 
@@ -74,9 +98,27 @@ namespace RPG.Characters {
 		IEnumerator AttackPlayer(){
 			state = State.attacking;
 			while (distanceToPlayer <= currentWeaponRange) {
+				if (abilities != null) {
+					float timeWaitForCast = timeWaitBetweenCasts;
+
+					bool isTimeToCast = (Time.time - timeLastCast) > timeWaitForCast;
+					if (isTimeToCast) {
+						isReadyToCastAbility = true;
+					}
+				}
 				weaponSystem.AttackTarget (player.gameObject);
 				yield return new WaitForEndOfFrame ();
 			}
+		}
+
+		IEnumerator CastAbility(){
+			state = State.castingAbility;
+			Debug.Log ("Channeling");
+			yield return new WaitForSeconds (3f);
+			timeLastCast = Time.time;
+			abilities [ABILITY_NUMBER_0].Use (null);
+			Debug.Log ("KKABBOOOOM");
+			isReadyToCastAbility = false;
 		}
 
 		void OnDrawGizmos(){
