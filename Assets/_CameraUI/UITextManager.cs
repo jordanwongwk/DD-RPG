22e3f022c51faed5a5fe389999522a1f381f43d7
Jordan Wong <jordanwongwk@gmail.com>
2018-06-04 14:11:04 +0800

    Done simple conversation UI and mechanics.

--- Assets/_CameraUI/UITextManager.cs
+++ Assets/_CameraUI/UITextManager.cs
@@ -3,49 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UITextManager : MonoBehaviour {
-	[SerializeField] GameObject skillDescriptionTextBar = null;
-	[SerializeField] GameObject questTrackerTextBar = null;
+namespace RPG.CameraUI {
+	public class UITextManager : MonoBehaviour {
+		[SerializeField] GameObject skillDescriptionTextBar = null;
+		[SerializeField] GameObject questTrackerTextBar = null;
+		[SerializeField] GameObject NPCConvoInstruction = null;
+		[SerializeField] GameObject convoTextBar = null;
 
-	Text skillText;
-	Text questText;
+		Text skillText;
+		Text questText;
+		Text instructionText;
+		Text convoText;
 
-	// Use this for initialization
-	void Start () {
-		skillDescriptionTextBar.SetActive (false);	
-		skillText = skillDescriptionTextBar.GetComponentInChildren<Text> ();
-		questText = questTrackerTextBar.GetComponentInChildren<Text> (); 
+		// Use this for initialization
+		void Start () {
+			skillDescriptionTextBar.SetActive (false);	
+			skillText = skillDescriptionTextBar.GetComponentInChildren<Text> ();
+			questText = questTrackerTextBar.GetComponentInChildren<Text> (); 
+			instructionText = NPCConvoInstruction.GetComponent<Text> ();
+			convoText = convoTextBar.GetComponentInChildren<Text> ();
 
-		Quest01 ();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+			Quest01 ();
+		}
 
-	}
+		// Conversation Tracker
+		public void ShowInstruction(){
+			NPCConvoInstruction.SetActive (true);
+			instructionText.text = "Press F to Talk";
+		}
 
-	// Quest Tracker
-	public void Quest01(){
-		questText.text = "- Head to the village on top of the hill to the west.\n- Deliver mysterious parcel to Derrick.";
-	}
+		public void ShowNPCTextBox(){
+			convoTextBar.SetActive (true);
+		}
 
-	// Skill Descriptions
-	public void MouseOverSkill1(){
-		skillDescriptionTextBar.SetActive (true);
-		skillText.text = "Explosion [Key-1] ; Energy Cost = 50 \nDeal explosive damage to nearby enemies.";
-	}
+		public void SetNPCConvoText(string NPCText){
+			convoText.text = NPCText;
+		}
 
-	public void MouseOverSkill2(){
-		skillDescriptionTextBar.SetActive (true);
-		skillText.text = "Heal [Key-2] ; Energy Cost = 25 \nRecover moderate amount of health.";
-	}
+		public void DisableInstructionAndNPCTextBox(){
+			NPCConvoInstruction.SetActive (false);
+			convoTextBar.SetActive (false);
+		}
+			
 
-	public void MouseOverSkillRM(){
-		skillDescriptionTextBar.SetActive (true);
-		skillText.text = "Power Attack [Key-3] ; Energy Cost = 25 \nDeliver a much powerful blow by using energy.";
-	}
 
-	public void MouseExit(){
-		skillDescriptionTextBar.SetActive (false);
+		// Quest Tracker
+		public void Quest01(){
+			questText.text = "- Head to the village on top of the hill to the west.\n- Deliver mysterious parcel to Derrick.";
+		}
+		// END Quest Tracker
+
+		// Skill Descriptions
+		public void MouseOverSkill1(){
+			skillDescriptionTextBar.SetActive (true);
+			skillText.text = "Explosion [Key-1] ; Energy Cost = 50 \nDeal explosive damage to nearby enemies.";
+		}
+
+		public void MouseOverSkill2(){
+			skillDescriptionTextBar.SetActive (true);
+			skillText.text = "Heal [Key-2] ; Energy Cost = 25 \nRecover moderate amount of health.";
+		}
+
+		public void MouseOverSkillRM(){
+			skillDescriptionTextBar.SetActive (true);
+			skillText.text = "Power Attack [Key-3] ; Energy Cost = 25 \nDeliver a much powerful blow by using energy.";
+		}
+
+		public void MouseExit(){
+			skillDescriptionTextBar.SetActive (false);
+		}
 	}
 }
---
+++ Assets/_Characters/NPCs/NPCConvoEvent.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using RPG.CameraUI;
+
+namespace RPG.Characters{
+	public class NPCConvoEvent : MonoBehaviour {
+
+		[SerializeField] int NPCIdentityID = 0;
+		[SerializeField] float distanceToPlayerToTrigger = 5f;
+
+		GameObject player;
+		UITextManager textManager;
+		List<string> NPCText = new List<string>();
+		int convoSequence;
+		bool startConversation = false;
+
+
+		void Start () {
+			player = FindObjectOfType<PlayerControl> ().gameObject;
+			textManager = FindObjectOfType<UITextManager> ();
+
+			SettingUpConvoText ();
+		}
+
+		void Update(){
+			float distanceDifferenceToPlayer = Vector3.Distance (transform.position, player.transform.position);
+			if (distanceDifferenceToPlayer <= distanceToPlayerToTrigger) {
+				textManager.ShowInstruction ();
+			} else if (distanceDifferenceToPlayer > distanceToPlayerToTrigger) {
+				textManager.DisableInstructionAndNPCTextBox ();
+			}
+
+			if (distanceDifferenceToPlayer <= distanceToPlayerToTrigger && Input.GetKeyDown (KeyCode.F)) {
+				if (!startConversation) {
+					StartConvoWithNPC ();
+					Debug.Log (NPCText.Count);
+				} else if (startConversation) {
+					convoSequence += 1;
+					ManageConversation ();
+				}
+			}
+		}
+
+		void ManageConversation ()
+		{
+			if (convoSequence >= NPCText.Count) {
+				EndConversation ();
+			} else {
+				textManager.SetNPCConvoText (NPCText [convoSequence]);
+			}
+		}
+
+		void EndConversation ()
+		{
+			startConversation = false;
+			textManager.DisableInstructionAndNPCTextBox ();
+			// Can Move
+		}
+
+		void StartConvoWithNPC(){
+			startConversation = true;
+			convoSequence = 0;
+			textManager.ShowNPCTextBox ();
+			textManager.SetNPCConvoText (NPCText [convoSequence]);
+			// Stop Moving Player
+		}
+
+		void SettingUpConvoText(){
+			if (NPCIdentityID == 0) {
+				NPCText.Add("Man:\n I'm feeling so tired.");
+				NPCText.Add("You:\n Take a break man, its a long day.");
+			}
+
+			if (NPCIdentityID == 1) {
+				Debug.Log ("HELP");
+			}
+		}
+
+		void OnDrawGizmos () {
+			Gizmos.color = new Color(0,0,255f, 0.5f);
+			Gizmos.DrawWireSphere (transform.position, distanceToPlayerToTrigger);
+		}
+	}
+}
