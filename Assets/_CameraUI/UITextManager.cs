1acc16537c8747d6f4deb923c2809740ab84ac5a
Jordan Wong <jordanwongwk@gmail.com>
2018-06-04 10:39:02 +0800

    Add a quest tracker (not fully functioning atm)

--- Assets/_CameraUI/UITextManager.cs
+++ Assets/_CameraUI/UITextManager.cs
@@ -5,20 +5,31 @@ using UnityEngine.UI;
 
 public class UITextManager : MonoBehaviour {
 	[SerializeField] GameObject skillDescriptionTextBar = null;
+	[SerializeField] GameObject questTrackerTextBar = null;
 
 	Text skillText;
+	Text questText;
 
 	// Use this for initialization
 	void Start () {
 		skillDescriptionTextBar.SetActive (false);	
 		skillText = skillDescriptionTextBar.GetComponentInChildren<Text> ();
+		questText = questTrackerTextBar.GetComponentInChildren<Text> (); 
+
+		Quest01 ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 	}
 
+	// Quest Tracker
+	public void Quest01(){
+		questText.text = "- Head to the village on top of the hill to the west.\n- Deliver mysterious parcel to Derrick.";
+	}
+
+	// Skill Descriptions
 	public void MouseOverSkill1(){
 		skillDescriptionTextBar.SetActive (true);
 		skillText.text = "Explosion [Key-1] ; Energy Cost = 50 \nDeal explosive damage to nearby enemies.";
