c65c8c4f8cefe0dab927ed63798ed3f522184e8e
Jordan Wong <jordanwongwk@gmail.com>
2018-06-08 12:20:27 +0800

    Checkpoint implementation Pt 1 : Able to make checkpoint lights up accordingly

---
+++ Assets/_Environment/_Checkpoints/Checkpoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Characters;
+
+public class Checkpoint : MonoBehaviour {
+
+	MeshRenderer meshRenderer;
+	CheckpointManager checkpointManager;
+
+	// Use this for initialization
+	void Start () {
+		meshRenderer = GetComponent<MeshRenderer> ();
+		checkpointManager = FindObjectOfType<CheckpointManager> ();
+	}
+	
+	public void SetCheckpointON (Material checkpointON) {
+		meshRenderer.material = checkpointON;
+	}
+
+	public void SetCheckpointOFF (Material checkpointOFF) {
+		meshRenderer.material = checkpointOFF;
+	}
+
+	void OnTriggerEnter(Collider col){
+		if (col.gameObject == FindObjectOfType<PlayerControl> ().gameObject) {
+			checkpointManager.SettingUpTheCheckpoint (this);
+		}
+	}
+}
---
+++ Assets/_Managers/CheckpointManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointManager : MonoBehaviour {
+
+	[SerializeField] Material checkpointON = null;
+	[SerializeField] Material checkpointOFF = null;
+
+	List<GameObject> checkpoints = new List<GameObject>();
+
+	GameObject currentCheckpointRegistered = null;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < transform.childCount; i++) {
+			checkpoints.Add (transform.GetChild (i).gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void SettingUpTheCheckpoint(Checkpoint currentCP){
+		foreach (GameObject cp in checkpoints) {
+			cp.GetComponent<Checkpoint> ().SetCheckpointOFF (checkpointOFF);
+		}
+		currentCheckpointRegistered = currentCP.gameObject;
+		currentCP.SetCheckpointON (checkpointON);
+	}
+}
