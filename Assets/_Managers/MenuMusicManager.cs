8743f34767bf0aeae2eed15493308f459b52e7e8
Jordan Wong <jordanwongwk@gmail.com>
2018-06-14 11:05:07 +0800

    Adding in fading effect and music using singleton in main menu

--- Assets/_Managers/MenuMusicManager.cs
+++ Assets/_Managers/MenuMusicManager.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuMusicManager : MonoBehaviour {
+	[SerializeField] AudioClip mainMenuTheme = null;
+	[SerializeField] float audioVolume = 0.6f;
+
+	MenuUIManager uiManager;
 	static MenuMusicManager instance = null;
 
 	// Use this for initialization
 	void Start () {
+		uiManager = FindObjectOfType<MenuUIManager> ();
+
 		if (instance != null) {
 			Destroy (gameObject);
 			return;
 		} else {
+			uiManager.PanelFadeIn ();
 			instance = this;
 		}
+
+		AudioSource audioSource;
+		audioSource = gameObject.AddComponent<AudioSource> ();
+		audioSource.volume = audioVolume;
+		audioSource.playOnAwake = true;
+		audioSource.loop = true;
+		audioSource.PlayOneShot (mainMenuTheme);
 			
 		DontDestroyOnLoad (gameObject);
 	}
---
+++ Assets/_Managers/MenuUIManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuUIManager : MonoBehaviour {
+	[SerializeField] GameObject gamePanel = null;
+
+	Color panelColor;
+	Image gamePanelBackground;
+	bool isPanelFadingIn = false;
+	const float FADE_TIME = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		gamePanelBackground = gamePanel.GetComponent<Image> ();
+		panelColor = gamePanelBackground.color;
+		panelColor.a = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isPanelFadingIn) {
+			StartCoroutine (FadingInPanel ());
+		} 
+	}
+
+	public void PanelFadeIn(){
+		gamePanel.SetActive (true);
+		isPanelFadingIn = true;
+	}
+
+	IEnumerator FadingInPanel(){
+		panelColor.a -= Time.deltaTime / FADE_TIME;
+		gamePanelBackground.color = panelColor;
+		yield return new WaitForSeconds (FADE_TIME);
+		if (isPanelFadingIn) {					// Need to call this so the other called FadingInPanel coroutine won't keep disable the fade panel
+			gamePanel.SetActive (false);
+		}
+		isPanelFadingIn = false;
+	}
+}
